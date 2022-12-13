d307a4ac91f84c124cc94c8b62f0b8c2c26a21dd
javohir000000 <120446959+javohir000000@users.noreply.github.com>
2022-12-12 23:00:16 -0800

    ;v,wvf
    mciefie

---
+++ App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Lenovo from "./components/lenovo/Lenovo"
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Hero/>
+     <Lenovo/>
+    </div>
+  );
+}
+
+export default App;
